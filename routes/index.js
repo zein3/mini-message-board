f5432f246f10a1c5620a3d0ed40cc9afa0523d90
zein3 <zeinhaddad02@gmail.com>
2022-08-01 06:27:16 +0700

    show time using luxon now

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { DateTime } = require('luxon');
 const Message = require('../models/message');
 
 router.get('/', async (req, res) => {
-  const messages = await Message.findAll({
-    order: [['updatedAt', 'DESC']]
+  const results = await Message.findAll({
+    order: [['updatedAt', 'DESC']],
+    raw: true
+  });
+
+  const messages = results.map(message => {
+    return {
+      ...message,
+      updatedAt: DateTime.fromJSDate(message.updatedAt).toLocaleString(DateTime.DATETIME_MED)
+    }
   });
 
   res.render('index', {
